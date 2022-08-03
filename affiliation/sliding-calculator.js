3048af4b77c168e4cf84c350ba26b9efc02e40dc
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2022-08-03 13:36:23 +0200

    l

--- affiliation/sliding-calculator.js
+++ affiliation/sliding-calculator.js
@@ -1,4 +1,6 @@
 
+/****Run everything on every change in one of the input through autentica_cacl()**/
+
 document.getElementById("tip_monthly").addEventListener("change", autentica_calc);
 document.getElementById("tip1").addEventListener("change", autentica_calc);
 document.getElementById("tip2").addEventListener("change", autentica_calc);
@@ -7,51 +9,136 @@ document.getElementById("tip4").addEventListener("change", autentica_calc);
 document.getElementById("tip5").addEventListener("change", autentica_calc);
 
 
+var monthly = 0
 
-var calculator__rows = document.getElementsByClassName("calculator__row")
-function pointer_slider_update(){
-    document.getElementById("slider-value-montlhy").innerHTML =document.getElementById("tip_monthly").value + "€"
-    document.getElementById("slider-value-1").innerHTML = "x "+document.getElementsByClassName("calculator__tip")[0].value
-    document.getElementById("slider-value-2").innerHTML = "x "+document.getElementsByClassName("calculator__tip")[1].value
-    document.getElementById("slider-value-3").innerHTML = "x "+document.getElementsByClassName("calculator__tip")[2].value
-    document.getElementById("slider-value-4").innerHTML = "x "+document.getElementsByClassName("calculator__tip")[3].value
-    document.getElementById("slider-value-5").innerHTML = "x "+document.getElementsByClassName("calculator__tip")[4].value
+var s1 = 0
+var s2 = 0
+var s3 = 0
+var s4 = 0
+var s5 = 0
 
+
+var l1_a = 0
+var l2_a = 0
+var l3_a = 0
+var l4_a = 0
+var l5_a = 0
+var result_a = 0
+
+
+var l1_r = 0
+var l2_r = 0
+var l3_r = 0
+var l4_r = 0
+var l5_r = 0
+var result_r = 0
+
+
+function format_user(number){
+    return Number(number).toLocaleString("es-ES", {minimumFractionDigits: 0});
+}
+
+
+function format_prize(number){
+    return new Intl.NumberFormat("de-DE", { style: "currency", "currency":"EUR" }).format(number);
+}
+
+function vars_def(){
+    monthly = document.getElementById("tip_monthly").value
+
+    s1 = document.getElementById("tip1").value
+    s2 = document.getElementById("tip2").value
+    s3 = document.getElementById("tip3").value
+    s4 = document.getElementById("tip4").value
+    s5 = document.getElementById("tip5").value
+
+    l1_a = (s1*10)
+    l2_a = (s1*s2*0.5) 
+    l3_a = (s1*s2*s3*1)
+    l4_a = (s1*s2*s3*s4*1.5)
+    l5_a = (s1*s2*s3*s4*s5*2)
+    result_a = l1_a + l2_a + l3_a + l4_a + l5_a
+
+    l1_r = (s1*(monthly/50))
+    l2_r = (s1*s2*(monthly/100))
+    l3_r = (s1*s2*s3*(monthly/100))
+    l4_r = (s1*s2*s3*s4*(monthly/100))
+    l5_r = (s1*s2*s3*s4*s5*(monthly/50))
+    result_r = l1_r + l2_r + l3_r + l4_r + l5_r
+}
+
+
+function slider_value_update(){
+
+    var monthly_s = document.getElementsByClassName("monthly") 
+    for (var i = 0; i < monthly_s.length; i++){
+    document.getElementsByClassName("monthly")[i].innerHTML = monthly + " €"
+    }
+
+    document.getElementById("slider-value-1").innerHTML = "x "+ s1
+    document.getElementById("slider-value-2").innerHTML = "x "+ s2
+    document.getElementById("slider-value-3").innerHTML = "x "+ s3
+    document.getElementById("slider-value-4").innerHTML = "x "+ s4
+    document.getElementById("slider-value-5").innerHTML = "x "+ s5
+}
+
+function tab_value_update(){
+/**odd lines first column (no 1st)**/
+    document.getElementById("tab1_m1").innerHTML = s1
+    document.getElementById("tab2_m1").innerHTML = s1
+
+    document.getElementById("tab1_m2").innerHTML = 'x'+s2
+    document.getElementById("tab2_m2").innerHTML = 'x'+s2
+
+    document.getElementById("tab1_m3").innerHTML = 'x'+s3
+    document.getElementById("tab2_m3").innerHTML = 'x'+s3
+
+    document.getElementById("tab1_m4").innerHTML = 'x'+s4
+    document.getElementById("tab2_m4").innerHTML = 'x'+s4
+
+    document.getElementById("tab1_m5").innerHTML = 'x'+s5
+    document.getElementById("tab2_m5").innerHTML = 'x'+s5
+
+/**event lines first column (no 2nd line)**/
+    document.getElementById("tab1_u2").innerHTML = format_user(s1*s2)
+    document.getElementById("tab2_u2").innerHTML = format_user(s1*s2)
+
+    document.getElementById("tab1_u3").innerHTML = format_user(s1*s2*s3)
+    document.getElementById("tab2_u3").innerHTML = format_user(s1*s2*s3)
+
+    document.getElementById("tab1_u4").innerHTML = format_user(s1*s2*s3*s4)
+    document.getElementById("tab2_u4").innerHTML = format_user(s1*s2*s3*s4)
+
+    document.getElementById("tab1_u5").innerHTML = format_user(s1*s2*s3*s4*s5)
+    document.getElementById("tab2_u5").innerHTML = format_user(s1*s2*s3*s4*s5)
+
+/**bottom line**/
+    var tot_utenti = Number(s1) + Number(s1*s2) + Number(s1*s2*s3) + Number(s1*s2*s3*s4) + Number(s1*s2*s3*s4*s5)
+    document.getElementById("tab1_u12345").innerHTML = format_user(tot_utenti)
+    document.getElementById("tab2_u12345").innerHTML = format_user(tot_utenti)
+
+/**last column**/
+    document.getElementById("tab1_p1").innerHTML = format_prize(l1_a)
+    document.getElementById("tab1_p2").innerHTML = format_prize(l2_a)
+    document.getElementById("tab1_p3").innerHTML = format_prize(l3_a)
+    document.getElementById("tab1_p4").innerHTML = format_prize(l4_a)
+    document.getElementById("tab1_p5").innerHTML = format_prize(l5_a)
 }
 
 
 function autentica_calc(){
-    pointer_slider_update()
-    var monthly = document.getElementById("tip_monthly").value
-    var s1 = document.getElementById("tip1").value
-    var s2 = document.getElementById("tip2").value
-    var s3 = document.getElementById("tip3").value
-    var s4 = document.getElementById("tip4").value
-    var s5 = document.getElementById("tip5").value
+    vars_def()
+    slider_value_update()
+    tab_value_update()
 
 
-    var result = 0
+    document.getElementsByClassName("autentica_result")[0].innerHTML = format_prize(result_a)
+    document.getElementsByClassName("autentica_result")[1].innerHTML = format_prize(result_a)
 
-    var result2 = 0
-
-    result = ((s1*10) + (s1*s2*0.5) + (s1*s2*s3*1) + (s1*s2*s3*s4*1.5) + (s1*s2*s3*s4*s5*2))
-    result2 = (s1*(monthly/50)) + (s1*s2*(monthly/100)) + (s1*s2*s3*(monthly/100)) + (s1*s2*s3*s4*(monthly/100)) + (s1*s2*s3*s4*s5*(monthly/50))
-
-    var result2_eur = new Intl.NumberFormat("de-DE", { style: "currency", "currency":"EUR" }).format(result2);
-    var result_eur = new Intl.NumberFormat("de-DE", { style: "currency", "currency":"EUR" }).format(result);
-
-
-
-    document.getElementsByClassName("rendita_result")[0].innerHTML = result2_eur
-    document.getElementsByClassName("rendita_result")[1].innerHTML = result2_eur
-    document.getElementsByClassName("autentica_result")[0].innerHTML = result_eur
-    document.getElementsByClassName("autentica_result")[1].innerHTML = result_eur
+    document.getElementsByClassName("rendita_result")[0].innerHTML = format_prize(result_r)
+    document.getElementsByClassName("rendita_result")[1].innerHTML = format_prize(result_r)
 
 }
 
 
 autentica_calc()
-
-
-
-/******1a definizione poi su call( autentica_calc()) def_var() assegna_var()**********/
